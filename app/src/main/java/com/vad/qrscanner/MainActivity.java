49f23446f0cfec771c9adf3699fb15cace161f6d
Vadim <gabderahmanov99@gmail.com>
2021-06-19 11:33:27 +0300

    delete links fragment_links_generation.xml

--- app/src/main/java/com/vad/qrscanner/MainActivity.java
+++ app/src/main/java/com/vad/qrscanner/MainActivity.java
@@ -1,42 +1,13 @@
 package com.vad.qrscanner;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.vad.qrscanner.GeneratorQr;
-import com.vad.qrscanner.R;
-import com.vad.qrscanner.fragments.LinksFragmentGeneration;
 import com.vad.qrscanner.fragments.LocationFragmentGeneration;
 import com.vad.qrscanner.fragments.PhoneFragmentGeneration;
 import com.vad.qrscanner.fragments.TextFragmentGeneration;
@@ -72,10 +43,6 @@ public class MainActivity extends AppCompatActivity{
 
                 case R.id.coord_nav:
                     selected = new LocationFragmentGeneration();
-                    break;
-
-                case R.id.links_nav:
-                    selected = new LinksFragmentGeneration();
                     break;
             }
 
--- app/src/main/java/com/vad/qrscanner/fragments/TextFragmentGeneration.java
+++ app/src/main/java/com/vad/qrscanner/fragments/TextFragmentGeneration.java
@@ -1,5 +1,7 @@
 package com.vad.qrscanner.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +9,38 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.vad.qrscanner.GeneratorQr;
 import com.vad.qrscanner.R;
+import com.vad.qrscanner.result.ResultQrActivity;
 
 public class TextFragmentGeneration extends Fragment {
 
+    private Button btnGenerate;
+    private EditText editText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_generation, container, false);
+        View v = inflater.inflate(R.layout.fragment_text_generation, container, false);
+        btnGenerate = (Button) v.findViewById(R.id.buttonTextGenerate);
+        editText = (EditText) v.findViewById(R.id.editTextText);
+
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = editText.getText().toString();
+                Bitmap bitmap = GeneratorQr.generate(str);
+                resultActivityStart(bitmap);
+            }
+        });
+        return v;
+    }
+
+    private void resultActivityStart(Bitmap bitmap){
+        Intent intentRes = new Intent(getContext(), ResultQrActivity.class);
+        intentRes.putExtra("result_qr", bitmap);
+        getContext().startActivity(intentRes);
     }
 }
