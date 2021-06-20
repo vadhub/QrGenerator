cb03209028ced794695e80d3677f32c4b87ea6d1
Vadim <gabderahmanov99@gmail.com>
2021-06-20 23:03:02 +0300

    permission access location

--- app/src/main/java/com/vad/qrscanner/fragments/LocationFragmentGeneration.java
+++ app/src/main/java/com/vad/qrscanner/fragments/LocationFragmentGeneration.java
@@ -5,8 +5,9 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,15 +15,41 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.vad.qrscanner.R;
 
 
 public class LocationFragmentGeneration extends Fragment {
 
     private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private void checkPermission(){
+        if (
+                ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+
+        } else {
+            requestPermissionLauncher.launch(
+                    Manifest.permission.REQUESTED_PERMISSION);
+        }
+
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,7 +75,8 @@ public class LocationFragmentGeneration extends Fragment {
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-
+                double lat = location.getLatitude();
+                double lon = location.getLongitude();
             }
         });
     }
