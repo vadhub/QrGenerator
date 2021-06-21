8597985f8ed3dadd930e1f9902ce3641af6f4323
Vadim <gabderahmanov99@gmail.com>
2021-06-21 11:44:26 +0300

    request turn on gps

--- app/src/main/java/com/vad/qrscanner/fragments/LocationFragmentGeneration.java
+++ app/src/main/java/com/vad/qrscanner/fragments/LocationFragmentGeneration.java
@@ -1,25 +1,40 @@
 package com.vad.qrscanner.fragments;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.vad.qrscanner.MainActivity;
 import com.vad.qrscanner.R;
@@ -27,8 +42,10 @@ import com.vad.qrscanner.R;
 
 public class LocationFragmentGeneration extends Fragment {
 
-    private static final int STORAGE_PERMISSION_CODE = 1103;
+    private static final int LOCATION_PERMISSION_CODE = 1103;
+    private static final int REQUEST_CHECK_SETTINGS = 3332;
     private Button btnGenerate;
+    private LocationManager mLocationManager;
 
     private FusedLocationProviderClient fusedLocationProviderClient;
 
@@ -56,7 +73,7 @@ public class LocationFragmentGeneration extends Fragment {
                     .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, STORAGE_PERMISSION_CODE);
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
                         }
                     }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                         @Override
@@ -65,10 +82,20 @@ public class LocationFragmentGeneration extends Fragment {
                         }
                     }).create().show();
         }else{
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, STORAGE_PERMISSION_CODE);
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_CODE);
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==LOCATION_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
+            }else{
+                Toast.makeText(getContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -77,6 +104,7 @@ public class LocationFragmentGeneration extends Fragment {
         View v = inflater.inflate(R.layout.fragment_location_generation, container, false);
         fusedLocationProviderClient = new FusedLocationProviderClient(getContext());
         btnGenerate = (Button) v.findViewById(R.id.buttonGenerationLocation);
+        mLocationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
 
         btnGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,8 +112,44 @@ public class LocationFragmentGeneration extends Fragment {
                 checkPermission();
             }
         });
-        checkPermission();
         return v;
+    }
+
+    private void requestGPSSettings() {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(getContext())
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationRequest.setInterval(2000);
+        locationRequest.setFastestInterval(500);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Log.i("", "All location settings are satisfied.");
+                        Toast.makeText(getContext(), "GPS is already enable", Toast.LENGTH_SHORT).show();
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.i("", "Location settings are not satisfied. Show the user a dialog to" + "upgrade location settings ");
+                        try {
+                            status.startResolutionForResult(getActivity(), REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.e("Applicationsett", e.toString());
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Toast.makeText(getContext(), "GPS unable", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 
     private void getLastLocation() {
