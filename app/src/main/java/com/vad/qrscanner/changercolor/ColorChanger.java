9c877bf46b49439f05d2ddbc561826374a41ea36
Vadim <gabderahmanov99@gmail.com>
2021-06-25 14:19:38 +0300

    redact custom view there is to add circle

--- app/src/main/java/com/vad/qrscanner/changercolor/ColorChanger.java
+++ app/src/main/java/com/vad/qrscanner/changercolor/ColorChanger.java
@@ -2,15 +2,35 @@ package com.vad.qrscanner.changercolor;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
+import android.widget.TextView;
 
 public class ColorChanger extends View {
+
+    private int color;
+    private Paint mPaint;
+
+
     public ColorChanger(Context context) {
         super(context);
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if(color==0){
+            color = Color.RED;
+        }
+        mPaint.setColor(color);
+        float cx, cy;
+        float radius = getHeight()/2;
+        cx = (float) (radius*2);
+        cy = radius;
+
+        canvas.drawCircle(cx, cy, radius, mPaint);
     }
 }
