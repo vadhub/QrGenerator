f8276a74e40caa1f164608ca35068e1a84e663d6
vadim <gabderahmanov99@gmail.com>
2023-01-25 15:11:54 +0300

    copy text

--- app/src/main/java/com/vad/qrscanner/fragments/ResultFragment.kt
+++ app/src/main/java/com/vad/qrscanner/fragments/ResultFragment.kt
@@ -1,14 +1,27 @@
 package com.vad.qrscanner.fragments
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.vad.qrscanner.R
 
+
 class ResultFragment : Fragment() {
+
+    private lateinit var thisContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.thisContext = context
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +35,17 @@ class ResultFragment : Fragment() {
 
         val result = view.findViewById(R.id.resultQr) as EditText
         result.setText(arguments?.getString("content"))
+
+        val copy = view.findViewById(R.id.copyImageView) as ImageView
+
+        copy.setOnClickListener {
+            val clipboard: ClipboardManager? =
+                thisContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+
+            val clip = ClipData.newPlainText("copyLable", result.text.toString())
+            clipboard!!.setPrimaryClip(clip)
+
+            Toast.makeText(thisContext, "text copied", Toast.LENGTH_SHORT).show()
+        }
     }
 }
