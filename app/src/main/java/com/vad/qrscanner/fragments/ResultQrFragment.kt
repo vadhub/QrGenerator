d489339f7803d3d930c66018eebbc41c1833d632
vadim <gabderahmanov99@gmail.com>
2023-02-08 12:35:19 +0300

    request permission on write

--- app/src/main/java/com/vad/qrscanner/fragments/ResultQrFragment.kt
+++ app/src/main/java/com/vad/qrscanner/fragments/ResultQrFragment.kt
@@ -13,6 +13,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -75,17 +76,31 @@ class ResultQrFragment : Fragment(), HasCustomTitle, HasCustomAction {
 
     override fun setCustomAction(navigator: Navigator) = CustomAction(R.drawable.ic_baseline_save_24) {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            val outFile: String = MediaStore.Images.Media.insertImage(
+            MediaStore.Images.Media.insertImage(
                 context?.contentResolver,
                 bitmapQr, "" + System.currentTimeMillis(), ""
             )
 
             Toast.makeText(context, getString(R.string.save_to) + "Gallery", Toast.LENGTH_LONG).show()
         } else {
-            val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            ActivityCompat.requestPermissions(requireActivity(), permissions, REQUEST_CODE)
+            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                MediaStore.Images.Media.insertImage(
+                    context?.contentResolver,
+                    bitmapQr, "" + System.currentTimeMillis(), ""
+                )
+                Toast.makeText(context, getString(R.string.save_to) + "Gallery", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(context, "Permission not granted", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun getTitle() = R.string.fragment_result_qr
 }
